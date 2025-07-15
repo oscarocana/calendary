2579e9eb253abc6247efe84d3027ced9299738bd
Oscar Ocana <email@oscarocana.com>
2025-07-15 18:46:37 +0100

    added majority of ScheduleTable schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,3 +1,4 @@
+import { unique } from "drizzle-orm/gel-core";
 import { pgTable, boolean, index, text, timestamp, uuid, integer } from "drizzle-orm/pg-core";
 
 // reusable code block that automatically sets the timestamp when the row is created (defualts to the current time)
@@ -22,4 +23,25 @@ export const EventTable = pgTable("events", {
     (table) => ([
         index("clerkUserIdIndex").on(table.clerkUserId),
     ])
+);
+
+// Defines the Schedules table schema, which is used to store the schedules for each user
+export const ScheduleTable = pgTable("schedules", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkUserId: text("clerkUserId").notNull().unique(),
+    clearkUserId: text("clearkUserId").notNull(),unique(),
+    createdAt,
+    updatedAt, 
+})
+
+// Defines the scheduleAvailability table schema, which is used to store the availability for each user
+export const ScheduleAvailabilityTable = pgTable("scheduleAvailability", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    scheduleId: uuid("scheduleId").notNull().references(() => ScheduleTable.id, {onDelete:"cascade"}),
+    startTime: timestamp("startTime").notNull(),
+    endTime: timestamp("endTime").notNull(),
+    dayOfWeek: scheduleDayOfWeekEnum("dayOfWeek").notNull(),
+    createdAt,
+    updatedAt,
+}
 );
