62a3d610783030d92743c335304d2e40a1d028b6
Oscar Ocana <email@oscarocana.com>
2025-07-21 17:26:09 +0100

    Add Mainlayout component with user authentication check

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs/server";
+import PubllicNavBar from "../components/PublicNavBar";
+import PriavteNavBar from "../components/PrivateNavBar";
+
+export default async function Mainlayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+    // Checks if the user is authenticated
+    const user = await currentUser();
+
+  return (
+    <main className="realtative">
+        {/* If the user is authenticated, display private Navbar, if not displays the public navbar */}
+        {user ? <PriavteNavBar /> : <PubllicNavBar />}
+        <section>
+           {children} 
+        </section>
+    </main>
+  );
+}
