9d2fb2838ada32d8e3759c91ca9c3c618b47dd01
Oscar Ocana <email@oscarocana.com>
2025-07-21 18:44:59 +0100

    Add Events page and refactor navigation components for improved structure and functionality

--- app/components/PrivateNavBar.tsx
+++ app/components/PrivateNavBar.tsx
@@ -1,49 +1,72 @@
-import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+"use client"
+
+import { PrivateNavLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { SignedIn, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export default function PubllicNavBar() {
+export default function PrivateNavBar() {
+    const pathname = usePathname();
+
     return (
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-lg font-bold">Calendary App</div>
-                <div>
-                    <a href="/" className="text-white hover:text-gray-300 px-3">Home</a>
-                    <a href="/about" className="text-white hover:text-gray-300 px-3">About</a>
-                    <a href="/contact" className="text-white hover:text-gray-300 px-3">Contact</a>
-                </div>
-                <Link href="/login" className="flex items-center gap-1 hover:scale-150 duration-500 ">
+        <nav className="flex justify-between items-center fixed z-50 w-full h-28 bg-gray-200 px-10 gap-4 shadow-2xl mb-28">
+              {/* Logo */}
+              <Link href="/events" className="flex items-center gap-1 hover:scale-150 duration-500 ">
                 <Image
-                            src='/assets/logo.svg'
-                            width={100}
-                            height={100}
-                            alt="logo"
-                        />
-                </Link>
-            </div>
+                  src="/assets/logo.svg"
+                  width={60}
+                  height={60}
+                  alt="Let's talk"
+                />
+              </Link>
 
-            {/* Nav Links */}
-            <section className="sticky top-0 flex justify-between  ">
+              {/* Nav Links */}
+              <section className="sticky top-0 flex justify-between text-black ">
                 <div className="flex flex-1 max-sm:gap-0 sm:gap-6">
-                <SignInButton>
-                    <Button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl"
-                    >
-                        Login
-                    </Button>
-                </SignInButton>
-                <SignUpButton>
-                    <Button
-                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl"
-                    variant={"outline"}
-                    >
-                        Register
-                    </Button>
-                </SignUpButton>
+                  {PrivateNavLinks.map((item) => {
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+                    
+                    return (
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={
+                          cn('flex gap-4 items-center p-4 rounded-lg justify-start hover:scale-150 duration-300 ',
+                            isActive && 'bg-blue-100 rounded-3xl'
+                          )
+                        }
+                      >
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={30}
+                          height={30}
+                        />
+                        
+                        
+                        
+                        <p className={cn(
+                            "text-lg font-semibold max-lg:hidden",
+                          )}>
+                          {item.label}
+                        </p>
+                      </Link>
+                    );
+                  })}
                 </div>
-            </section>  
+              </section>    
+
+              {/* User button */}
+              <div className='hover:scale-150 duration-500 '>
+                <SignedIn>
+                    {/* Mount the UserButton component */}
+                    <UserButton />
+                </SignedIn>
+        
+              </div>    
 
         </nav>
-    );
+    )
 }
--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,21 @@ export const DAYS_OF_WEEK = [
     "Saturday",
     "Sunday",
 ] as const;
+
+export const PrivateNavLinks = [
+{
+    imgURL: '/assets/events.svg',
+    route: '/events',
+    label: 'My Events',
+},
+{
+    imgURL: '/assets/schedule.svg',
+    route: '/schedule',
+    label: 'My Schedule',
+},
+{
+    imgURL: '/assets/public.svg',
+    route: '/book',
+    label: 'Public Profile',
+},
+] as const
