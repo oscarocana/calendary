1badcd2c6d7dabad502c9f47db17f14bbe1e24af
Oscar Ocana <email@oscarocana.com>
2025-07-21 17:36:30 +0100

    Fix link wrapping for logo in PrivateNavBar component

--- app/components/PrivateNavBar.tsx
+++ app/components/PrivateNavBar.tsx
@@ -1,4 +1,6 @@
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function PubllicNavBar() {
     return (
@@ -10,13 +12,37 @@ export default function PubllicNavBar() {
                     <a href="/about" className="text-white hover:text-gray-300 px-3">About</a>
                     <a href="/contact" className="text-white hover:text-gray-300 px-3">Contact</a>
                 </div>
+                <Link href="/login" className="flex items-center gap-1 hover:scale-150 duration-500 ">
                 <Image
                             src='/assets/logo.svg'
                             width={100}
                             height={100}
                             alt="logo"
                         />
+                </Link>
             </div>
+
+            {/* Nav Links */}
+            <section className="sticky top-0 flex justify-between  ">
+                <div className="flex flex-1 max-sm:gap-0 sm:gap-6">
+                <SignInButton>
+                    <Button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl"
+                    >
+                        Login
+                    </Button>
+                </SignInButton>
+                <SignUpButton>
+                    <Button
+                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl"
+                    variant={"outline"}
+                    >
+                        Register
+                    </Button>
+                </SignUpButton>
+                </div>
+            </section>  
+
         </nav>
     );
 }
