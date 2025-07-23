a9ca955ffe203cd895bd28864f038b51386bd578
Oscar Ocana <email@oscarocana.com>
2025-07-23 19:10:33 +0100

    uncommented out <form> tag

--- app/(main)/(private)/events/new_event/page.tsx
+++ app/(main)/(private)/events/new_event/page.tsx
@@ -1,6 +1,7 @@
 // This code defines a React component called `NewEventPage`, which renders a page for creating a new event. It uses a card layout to display a centered form on the screen. The card includes a header with the title "New Event" and a content section that contains the `EventForm` component, which likely handles the user input and submission logic for creating a new calendar event. The layout is styled using utility classes to make it visually appealing and responsive, ensuring a clean and focused user interface for event creation.
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import EventForm from "@/components/ui/forms/EventForms";
 
 
 export default function NewEventPage(){
@@ -14,7 +15,7 @@ export default function NewEventPage(){
     
           {/* Content section of the card containing the event form */}
           <CardContent>
-            {/* <EventForm /> */}
+            <EventForm />
           </CardContent>
         </Card>
       )
