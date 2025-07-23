3eed4e39d861a8d40586620df0317cc76359db9a
Oscar Ocana <email@oscarocana.com>
2025-07-23 21:27:28 +0100

    created the event card to display a list of events

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -1,6 +1,9 @@
 import { cn } from "@/lib/utils";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
 import { formatEventDescription } from "@/lib/formatters";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { CopyEventButton } from "../CopyEventButton";
 
 type EventCardProps = {
     id: string; // The unique identifier for the event
