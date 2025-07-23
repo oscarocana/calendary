c6e6b454f81f69ff0c0caa5f6ece19b91ef365fb
Oscar Ocana <email@oscarocana.com>
2025-07-23 21:02:45 +0100

    added import for formatEventDescription

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import { formatEventDescription } from "@/lib/formatters";
 
 type EventCardProps = {
     id: string; // The unique identifier for the event
