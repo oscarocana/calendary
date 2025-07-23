3ffc5343be82163dc09e47a9f1233db4035a727b
Oscar Ocana <email@oscarocana.com>
2025-07-23 18:52:46 +0100

    added updateEvent function

--- server/actions/events.ts
+++ server/actions/events.ts
@@ -3,6 +3,7 @@ import { db } from "@/drizzle/db";
 import { EventTable } from "@/drizzle/schema";
 import { eventFormSchema } from "@/schema/events";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import z from "zod";
@@ -28,5 +29,33 @@ export async function createEvent(
     } finally {
         revalidatePath("/events") // Revalidates the path to ensure the latest data is fetched drom the server
         redirect("/events") // Redirects the user to the events page after event creation
+    }
+}
+
+export async function updateEvent(
+    id: string, // The ID of the event to update
+    unsafeData: z.infer<typeof eventFormSchema> // The data type is inferred and validated from the Zod schema
+): Promise<void> {
+    try {
+        const { userId } = await auth() // Authenticates the user and retrieves their ID
+        const {success, data} = eventFormSchema.safeParse(unsafeData); // Validates the data against the eventFormSchema
+        if (!success || !userId) {
+            throw new Error("Invalid event data or user not authenticated");
+        }
+
+        // If validation is successful, proceed to update the event in the DB, ensuring it belongs to the userId
+        const { rowCount } = await db.update(EventTable).set({...data}).where(and(eq(EventTable.id, id), eq(EventTable.clerkUserId, userId)))
+        
+        // Checks if the event exist 
+        if (rowCount === 0) {
+            throw new Error("Event not found or you do not have permission to update it");
+        }
+    
+    } catch (err: any) {
+        // If any error occurs, throws a new error with a message
+        throw new Error(`Failed to update event: ${err.message || err}`);
+    } finally {
+        revalidatePath("/events") // Revalidates the path to ensure the latest data is fetched from the server
+        redirect("/events") // Redirects the user to the events page after event update
     }
 }
