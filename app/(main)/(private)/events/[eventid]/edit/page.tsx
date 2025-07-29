29bcd84df3083d3da08783c0077531b1740dbaca
Oscar Ocana <email@oscarocana.com>
2025-07-29 18:06:56 +0100

    reworked function

--- app/(main)/(private)/events/[eventid]/edit/page.tsx
+++ app/(main)/(private)/events/[eventid]/edit/page.tsx
@@ -1,13 +1,37 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import EventForm from "@/components/ui/forms/EventForms"
+import { getEvent } from "@/server/actions/events"
+import { auth } from "@clerk/nextjs/server"
 
-export default function EditEventPage({ 
-    params }: { params: Promise<{ eventId : string }> }) {
-    
-    const (userId, redirectToSignIn) = await auth();
-    if (!userId) return RedirectToSignIn(); // Redirect to sign-in if user is not authenticated
+// The default exported async function for the EditEventPage
+export default async function EditEventPage({
+  params,// Extracting the eventId from the URL params
+}: {
+  params: Promise<{ eventId: string }>
+}) {
+    // Authenticates the user and retrieves their ID
+  const { userId, redirectToSignIn } = await auth()
+  if (!userId) return redirectToSignIn() // If userId is not available, redirect to sign-in page
 
-    const { eventId } = await params; // Extract the event ID from the parameters
-    const event = await getEvent(eventId, userId); // Fetch the event details using the event ID and user ID
+  const { eventId } = await params
+    // Fetch the event details using the getEvent function
+  const event = await getEvent( userId,eventId )
+  if(!event) return <h1>Event not found</h1>
+
+    // Render the page with a card layout, displaying the "Edit Event" form
+  return (
+    <Card className="max-w-md mx-auto border-4 border-blue-100 shadow-2xl shadow-accent-foreground">
+      <CardHeader>
+        <CardTitle>Edit Event</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {/* Render the EventForm with the event details, passing the event data as props */}
+        <EventForm
+          event={{ ...event, description: event.description || undefined }} // If description is null, pass undefined
+        />
+      </CardContent>
+    </Card>
+  )
+
 
 }
