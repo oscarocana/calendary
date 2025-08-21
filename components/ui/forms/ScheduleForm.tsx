d1ad9bfeef31ba743c63d0c64c6a12025050d6a9
Oscar Ocana <email@oscarocana.com>
2025-08-21 18:25:04 +0100

    added a section on the form for availability (and the ability to add a remove slots)

--- components/ui/forms/ScheduleForm.tsx
+++ components/ui/forms/ScheduleForm.tsx
@@ -4,11 +4,15 @@ import { DAYS_OF_WEEK } from "@/constants";
 import { timeToFloat } from "@/lib/utils";
 import { scheduleFormSchema } from "@/schema/schedule";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import z from "zod";
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../form";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../select";
 import { formatTimezoneOffset } from "@/lib/formatters";
+import { Fragment } from "react";
+import { Button } from "../button";
+import { Plus, X } from "lucide-react";
+import { Input } from "../input";
 
 type Availability = {
     startTime: string
@@ -35,6 +39,19 @@ export function ScheduleForm({
             }),
             },    
     });
+
+
+const {
+    append: addAvailability, // Add a new availability entry
+    remove: removeAvailability, // Remove availability entry
+    fields: availabilityFields, // Current availability fields
+  } = useFieldArray({ name: "availabilities", control: form.control })
+
+        // Groups the available slot by each dayof the week for UI rendering
+    const groupedAvailabilityFields = Object.groupBy(
+        availabilityFields.map((field, index) => ({ ...field, index })),
+        availability => availability.dayOfWeek
+        )
 
     return (
         <form {...form}>
@@ -76,6 +93,121 @@ export function ScheduleForm({
                             </FormItem>
                         )}
                     />
+            
+                    {/* displays Availability on the form grouped by day */}
+                    <div className="grid grid-cols-[auto_auto]  gap-y-6">
+                {DAYS_OF_WEEK.map(dayOfWeek => (
+                <Fragment key={dayOfWeek}>
+                    {/* Day label */}
+                    <div className="capitalize text-sm font-semibold">
+                    {dayOfWeek.substring(0, 3)}
+                    </div>
+
+                    {/* Button to add availability for a specific day */}
+                    <div className="flex flex-col gap-2">
+                    <Button
+                        type="button"
+                        className="size-6 p-1 cursor-pointer hover:scale-200"
+                        variant="outline"
+                        onClick={() => {
+                        addAvailability({
+                            dayOfWeek,
+                            startTime: "9:00",
+                            endTime: "17:00",
+                        })
+                        }}
+                    >
+                        <Plus  color="green" />
+                    </Button>
+
+                    {/* Renders all availability entries for this day */}
+                        {groupedAvailabilityFields[dayOfWeek]?.map(
+                            (field, labelIndex) => (
+                            <div className="flex flex-col gap-1" key={field.id}>
+                                <div className="flex gap-2 items-center">
+                                {/* input for the availability Start time*/}
+                                <FormField
+                                    control={form.control}
+                                    name={`availabilities.${field.index}.startTime`}
+                                    render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                        <Input
+                                            className="w-24"
+                                            aria-label={`${dayOfWeek} Start Time ${
+                                            labelIndex + 1
+                                            }`}
+                                            {...field}
+                                        />
+                                        </FormControl>
+                                    </FormItem>
+                                    )}
+                                />
+                                -
+                                {/* input for the availability End time */}
+                                <FormField
+                                    control={form.control}
+                                    name={`availabilities.${field.index}.endTime`}
+                                    render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                        <Input
+                                            className="w-24"
+                                            aria-label={`${dayOfWeek} End Time ${
+                                            labelIndex + 1
+                                            }`}
+                                            {...field}
+                                        />
+                                        </FormControl>
+                                    </FormItem>
+                                    )}
+                                />
+
+                                {/* Removes the availability */}
+                                <Button
+                                    type="button"
+                                    className="size-6 p-1 cursor-pointer hover:bg-red-900"
+                                    variant="destructive"
+                                    onClick={() => removeAvailability(field.index)}
+                                >
+                                    <X />
+                                </Button>
+                                </div>
+
+                                {/* Shows validation messages */}
+                                <FormMessage>
+                                {
+                                    form.formState.errors.availabilities?.at?.(
+                                    field.index
+                                    )?.root?.message
+                                    
+                                }
+                                </FormMessage>
+                                <FormMessage>
+                                {
+                                    form.formState.errors.availabilities?.at?.(
+                                    field.index
+                                    )?.startTime?.message
+                                }
+                                </FormMessage>
+                                <FormMessage>
+                                {
+                                    form.formState.errors.availabilities?.at?.(
+                                    field.index
+                                    )?.endTime?.message
+                                }
+                                </FormMessage>
+                            </div>
+                            )
+                                )}
+                                </div>
+                            </Fragment>
+                            ))}
+                    </div>      
+            
+            
+            
+            
             </form>
 
         </form>
